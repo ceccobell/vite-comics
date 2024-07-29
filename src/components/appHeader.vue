4584185dd471fcbe18dde7072cdaa7def621af19
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-29 16:02:08 +0200

    [UPD] created component appHeader with stati style

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -5,9 +5,38 @@ export default {
 </script>
 
 <template>
-    
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center p-2">
+                    <img src="/public/img/dc-logo.png" alt="" class="img-fluid">
+                    <div class="h-100">
+                        <ul class="d-flex align-items-center list-unstyled h-100">
+                            <li><a href="#" class="text-decoration-none text-black me-3">CHARACTERS</a></li>
+                            <li class="active me-3"><a href="#" class="text-decoration-none">COMICS</a></li>
+                            <li><a href="#" class="text-decoration-none text-black me-3">MOVIES</a></li>
+                            <li><a href="#" class="text-decoration-none text-black me-3">TV</a></li>
+                            <li><a href="#" class="text-decoration-none text-black me-3">GAMES</a></li>
+                            <li><a href="#" class="text-decoration-none text-black me-3">COLLECTIBLES</a></li>
+                            <li><a href="#" class="text-decoration-none text-black me-3">VIDEOS</a></li>
+                            <li><a href="#" class="text-decoration-none text-black me-3">FANS</a></li>
+                            <li><a href="#" class="text-decoration-none text-black me-3">NEWS</a></li>
+                            <li><a href="#" class="text-decoration-none text-black">SHOP</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style scoped>
+.h-100 {
+    height: 100%;
+}
+.active {
+    color: #0282F9 !important;
+    border-bottom: 3px solid #0282F9;
+}
 
 </style>
