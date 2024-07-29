81f30b069f8a9f1c928f655c1281bc997f9dfba5
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-29 21:03:24 +0200

    [UPD] added section exploreDC

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -71,16 +71,18 @@ export default {
 
 <template>
     <header>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-12 d-flex justify-content-between align-items-center p-2">
-                    <img src="/public/img/dc-logo.png" alt="" class="img-fluid">
-                    <div class="h-100">
-                        <ul class="d-flex align-items-center list-unstyled h-100">
-                            <li v-for="item, index in menuItems" :key="index">
-                                <a @click="setActive(item)" :href="item.url" :class="item.active ? 'active' : ''" class="text-decoration-none text-black me-3">{{ item.name }}</a>
-                            </li>
-                        </ul>
+                <div class="col-12 p-2">
+                    <div class="content justify-content-between align-items-center">
+                        <img src="/public/img/dc-logo.png" alt="" class="img-fluid">
+                        <div class="h-100">
+                            <ul class="d-flex align-items-center list-unstyled h-100">
+                                <li v-for="item, index in menuItems" :key="index">
+                                    <a @click="setActive(item)" :href="item.url" :class="item.active ? 'active' : ''" class="text-decoration-none text-black me-3">{{ item.name }}</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -95,6 +97,12 @@ export default {
 .active {
     color: #0282F9 !important;
     border-bottom: 3px solid #0282F9;
+}
+
+.content {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
 }
 
 </style>
--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,15 +1,81 @@
 <script>
 export default {
-    
+    data() {
+        return {
+            exploreDC: [
+                {
+                    name: 'DIGITAL COMICS',
+                    img: '/public/img/buy-comics-digital-comics.png',
+                    url: '#',
+                    active: false
+                },
+                {
+                    name: 'DC MERCHANDISE',
+                    img: '/public/img/buy-comics-merchandise.png',
+                    url: '#',
+                    active: false
+                },
+                {
+                    name: 'SUBSCRIPTION',
+                    img: '/public/img/buy-comics-subscriptions.png',
+                    url: '#',
+                    active: false
+                },
+                {
+                    name: 'COMIC SHOP LOCATOR',
+                    img: '/public/img/buy-comics-shop-locator.png',
+                    url: '#',
+                    active: false
+                },
+                {
+                    name: 'DC POWER VISA',
+                    img: '/public/img/buy-dc-power-visa.svg',
+                    url: '#',
+                    active: false
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <main>
-        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 bg-black">
+                    <div class="content py-4 align-items-center">
+                        <h1 class="text-white fs-3">-> Content goes here <--</h1>
+                    </div>
+                </div>
+                <div class="col-12 bg-blue">
+                    <div class="content justify-content-center py-4">
+                        <ul class="d-flex align-items-center list-unstyled m-0 p-0">
+                            <li v-for="item in exploreDC" :key="item.name" class="mx-3 d-flex align-items-center">
+                                <img :src="item.img" alt="" class="img-fluid me-2">
+                                <a :href="item.url" class="text-decoration-none text-white">{{ item.name }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <style scoped>
+.content {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+}
 
+.bg-blue {
+    background-color: #0282F9;
+}
+
+.content img {
+    height: 30px;
+    width: auto;
+}
 </style>
