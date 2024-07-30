1076091cb575fddc8d78fd610dc973bef5d4ee60
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-30 15:25:29 +0200

    [UPD] added card dinamically without props

---
+++ src/components/cards.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
