8b8d26452498216f0b4b7fdcb81f16ee10892851
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-30 09:36:48 +0200

    [UPD] added bottom-footer

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -6,68 +6,88 @@ export default {
 
 <template>
     <footer>
-        <div class="container top-footer">
-            <div class="row">
-                <div class="col-6">
-                    <div class="content">
-                        <div class="col-4">
-                            <h4 class="text-white my-3">DC COMICS</h4>
-                            <ul class="list-unstyled">
-                                <li><a href="" class="text-decoration-none text-secondary">Characters</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Comics</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Movies</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">TV</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Games</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Videos</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">News</a></li>
-                            </ul>
-                            <h4 class="text-white my-3">SHOP</h4>
-                            <ul class="list-unstyled">
-                                <li><a href="" class="text-decoration-none text-secondary">Shop DC</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Shop DC Collectibles</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-4">
-                            <h4 class="text-white my-3">DC</h4>
-                            <ul class="list-unstyled">
-                                <li><a href="" class="text-decoration-none text-secondary">Terms Of Use</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Privacy policy(New)</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Ad Choices</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Advertising</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Jobs</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Subscriptions</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Talent Workshops</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">CPSC Certificates</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Ratings</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Shop Help</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">Contact Us</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-4">
-                            <h4 class="text-white my-3">SITES</h4>
-                            <ul class="list-unstyled">
-                                <li><a href="" class="text-decoration-none text-secondary">DC</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">MAD magazine</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">DC kids</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">DC Universe</a></li>
-                                <li><a href="" class="text-decoration-none text-secondary">DC Power Visa</a></li>
-                            </ul>
+        <div class="top-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="content">
+                            <div class="col-4">
+                                <h4 class="text-white my-3">DC COMICS</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="" class="text-decoration-none text-secondary">Characters</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Comics</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Movies</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">TV</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Games</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Videos</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">News</a></li>
+                                </ul>
+                                <h4 class="text-white my-3">SHOP</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="" class="text-decoration-none text-secondary">Shop DC</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Shop DC Collectibles</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h4 class="text-white my-3">DC</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="" class="text-decoration-none text-secondary">Terms Of Use</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Privacy policy(New)</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Ad Choices</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Advertising</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Jobs</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Subscriptions</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Talent Workshops</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">CPSC Certificates</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Ratings</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Shop Help</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">Contact Us</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h4 class="text-white my-3">SITES</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="" class="text-decoration-none text-secondary">DC</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">MAD magazine</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">DC kids</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">DC Universe</a></li>
+                                    <li><a href="" class="text-decoration-none text-secondary">DC Power Visa</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-6 bg-image">
-                    
+                    <div class="col-6 bg-image"></div>
                 </div>
             </div>
         </div>
-       
+        <div class="bottom-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-between align-items-center p-3">
+                        <div class="text-white border-blue">SIGN-UP NOW!</div>
+                        <div class="d-flex align-items-center">
+                            <div class="text-blue mx-2">FOLLOW US</div>
+                            <img src="/public/img/footer-facebook.png" alt="" class="mx-2">
+                            <img src="/public/img/footer-twitter.png" alt="" class="mx-2">
+                            <img src="/public/img/footer-youtube.png" alt="" class="mx-2">
+                            <img src="/public/img/footer-pinterest.png" alt="" class="mx-2">
+                            <img src="/public/img/footer-periscope.png" alt="" class="mx-2">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 
 <style scoped>
-footer {
+.top-footer {
     background-image: url("/public/img/footer-bg.jpg");
     background-size: cover;
+}
+
+.bottom-footer {
+    background-color: #303030;
 }
 
 .content {
@@ -81,5 +101,14 @@ footer {
   background-size: cover;
   background-position: center;
   height: 350px;
+}
+
+.text-blue {
+    color: #0282F9;
+}
+
+.border-blue {
+    border: 2px solid #0282F9;
+    padding: 10px;
 }
 </style>
