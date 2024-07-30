da337c12254c8616902d9d09a6c97b5d94d7ecba
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-30 16:05:47 +0200

    [UPD] load version with data loading via props

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script>
 import appHeader from './components/appHeader.vue';
 import appMain from './components/appMain.vue';
-import appFooter from './components/appFooter.vue'
+import appFooter from './components/appFooter.vue';
 
 export default {
   components: {
     appHeader,
     appMain,
-    appFooter
+    appFooter,
   }
 }
 </script>
--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,18 +1,23 @@
 <template>
-  
+    <div class="col-2 p-3" v-for="card, index in cards">
+        <img :src="card.thumb" alt="" class="img-card">
+        <h6 class="text-white text-uppercase mt-2">{{ card.series }}</h6>
+    </div>
 </template>
 
 <script>
 export default {
   props: {
-    items: {
-      type: Array,
-      required: true
-    }
+    cards: Array
   }
 }
 </script>
 
 <style scoped>
-
+.img-card {
+    width: 100%;
+    height: 250px;
+    object-fit: cover;
+    object-position: top;
+}
 </style>
